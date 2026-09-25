ab03735fc17ce4228c68133de05c2ca51c42fb9d
Makoetla Hopolang <hopzimakoetla@gmail.com>
2026-09-26 01:07:44 +0200

    Create Q2BasicArithmeticOperations.java
    This project shows how to do basic Math, it takes two numbers and prints their sum, difference, product, multiplication results and division results.

---
+++ Q2.BasicArithmeticOperations/src/q2/basicarithmeticoperations/Q2BasicArithmeticOperations.java
@@ -0,0 +1,24 @@
+package q2.basicarithmeticoperations;
+
+public class Q2BasicArithmeticOperations {
+
+    public static void main(String[] args) {
+        int a = 125;
+        int b = 24;
+        
+        int Sum =a+b;
+        int difference =a-b;
+        int product = a*b;
+        int quotient =a/b;
+        int remainder =a%b;
+        
+        System.out.println("Sum: "+ Sum);
+        System.out.println("Difference: "+ difference);
+        System.out.println("Product: "+ product);
+        System.out.println("Quotient "+ quotient);
+        System.out.println("Remainder "+remainder);
+        
+         
+    }
+    
+}
